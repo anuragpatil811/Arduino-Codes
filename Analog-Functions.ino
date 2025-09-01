8fcf5f124a4552ce6faf5e347eb5f8dea4c53883
Anurag Arun patil <anuragpatil811@gmail.com>
2025-09-01 22:25:21 +0530

    Add files via upload

---
+++ Analog-Functions.ino
@@ -0,0 +1,19 @@
+//Analog Read
+/*void setup() 
+{
+Serial.begin(9600);
+}
+
+void loop() {
+  int val = analogRead(A0);
+   Serial.println(val);
+   delay(1000);
+} */
+void setup()
+{
+  
+}
+void loop()
+{
+  analogWrite(6, 255);
+}
